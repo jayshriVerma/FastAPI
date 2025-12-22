b4a105cf8a125019d1fa4b7d8fc27424490f4251
jayshriVerma <jayshriyog@gmail.com>
2025-12-22 23:18:04 +0000

    use pydanticv2 modelconfig

--- settings.py
+++ settings.py
@@ -1,12 +1,13 @@
 from typing import Dict
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     redis_url: str
     VALID_API_KEYS: Dict[str, str]
 
-    class Config:
-        env_file = ".env"
+    model_config = SettingsConfigDict(
+        env_file = ".env",
+        )
 
 settings = Settings()
